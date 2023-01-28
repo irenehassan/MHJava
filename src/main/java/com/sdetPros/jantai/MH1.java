67d690f6f481131be0b889147755468a714d295b
jantai1 <jantaimonol@gmail.com>
2023-01-27 20:18:08 -0500

    add second class

--- src/main/java/com/sdetPros/jantai/MH1.java
+++ src/main/java/com/sdetPros/jantai/MH1.java
@@ -1,2 +1,11 @@
-package com.sdetPros.jantai;public class MH1 {
+package com.sdetPros.jantai;
+
+import java.util.logging.Logger;
+
+public class MH1 {
+
+    public static void main(String[] args) {
+        System.out.println("hey!" +
+                "");
+    }
 }
